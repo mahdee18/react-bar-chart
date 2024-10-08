44d358a9ba1d6e1e9f2a102fe9a550f85db5e0f9
mahdee18 <mahdeec10@gmail.com>
2024-10-08 15:25:45 +0600

    Make header title responsive for various screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className='text-green-500 text-4xl font-bold text-center py-10'>React Bar Chart Task</h1>
+        <h1 className="text-green-500 text-3xl md:text-4xl lg:text-5xl font-bold text-center py-6 md:py-10">
+          React Bar Chart Task
+        </h1>
       </header>
       <BarChart></BarChart>
     </div>
